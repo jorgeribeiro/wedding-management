aa238e0d4ede4dae85745612757ef05d5ffde7b3
Jorge Ribeiro <joorgemelo@gmail.com>
2020-11-17 21:56:07 +0400

    Started form to add new invitations

--- src/components/InvitationForm.vue
+++ src/components/InvitationForm.vue
@@ -1,151 +1,75 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+  <v-card elevation="1" max-width="500" class="mx-auto">
+    <v-card-title>
+      <span class="headline">Nomes dos convidados</span>
+    </v-card-title>
 
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+    <v-card-text>
+      <v-form ref="form" lazy-validation>
+        <v-text-field
+          v-for="textField in textFields"
+          :key="textField.id"
+          :label="textField.label"
+          :v-model="textField.id"
+          :rules="nameRules"
+          required
+        ></v-text-field>
+      </v-form>
+    </v-card-text>
 
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+    <v-card-actions>
+      <v-btn @click="addNameTextField"> + nome </v-btn>
+      <v-btn v-show="showRemoveButton" @click="removeNameTextField">
+        - nome
+      </v-btn>
+      <v-btn color="success"> Salvar </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-  export default {
-    name: 'HelloWorld',
+export default {
+  name: "InvitationForm",
 
-    data: () => ({
-      ecosystem: [
+  data() {
+    return {
+      textFields: [
         {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
+          id: "name1",
+          label: "Nome #1",
         },
       ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
+      numberOfTextFields: 1,
+      showRemoveButton: false,
+      nameRules: [
+        v => !!v || 'Campo obrigatório',
       ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
-    }),
-  }
+    };
+  },
+
+  methods: {
+    addNameTextField() {
+      ++this.numberOfTextFields;
+      this.showRemoveButton = true;
+
+      this.textFields.push({
+        id: "name" + this.numberOfTextFields,
+        label: "Nome #" + this.numberOfTextFields,
+      });
+    },
+
+    removeNameTextField() {
+      --this.numberOfTextFields;
+
+      this.textFields.pop({
+        id: "name" + this.numberOfTextFields,
+        label: "Nome #" + this.numberOfTextFields,
+      });
+
+      if (this.numberOfTextFields == 1) {
+        this.showRemoveButton = false;
+      }
+    },
+  },
+};
 </script>
