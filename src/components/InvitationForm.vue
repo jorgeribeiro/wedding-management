dadc0cacdb5294115029ef01da84768b6e820ef1
Jorge Ribeiro <joorgemelo@gmail.com>
2020-12-05 18:56:55 +0400

    Persist invitation

--- src/components/InvitationForm.vue
+++ src/components/InvitationForm.vue
@@ -8,9 +8,9 @@
       <v-card-text>
         <v-text-field
           v-for="textField in textFields"
+          v-model="textField.value"
           :key="textField.id"
           :label="textField.label"
-          :v-model="textField.id"
           :rules="nameRules"
           required
         ></v-text-field>
@@ -21,13 +21,15 @@
         <v-btn v-show="showRemoveButton" @click="removeNameTextField">
           - nome
         </v-btn>
-        <v-btn color="primary"> Salvar </v-btn>
+        <v-btn color="primary" @click="save"> Salvar </v-btn>
       </v-card-actions>
     </v-card>
   </v-form>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "InvitationForm",
 
@@ -37,6 +39,7 @@ export default {
         {
           id: "name1",
           label: "Nome #1",
+          value: "",
         },
       ],
       numberOfTextFields: 1,
@@ -53,6 +56,7 @@ export default {
       this.textFields.push({
         id: "name" + this.numberOfTextFields,
         label: "Nome #" + this.numberOfTextFields,
+        value: "",
       });
     },
 
@@ -61,12 +65,27 @@ export default {
 
       this.textFields.pop({
         id: "name" + this.numberOfTextFields,
-        label: "Nome #" + this.numberOfTextFields,
       });
 
       if (this.numberOfTextFields == 1) {
         this.showRemoveButton = false;
       }
+    },
+
+    save() {
+      let family = [];
+
+      this.textFields.forEach(function(entry) {
+        family.push(entry.value);
+      });
+
+      axios.post(process.env.VUE_APP_API_URL + "/invitation", {
+        "family": family,
+      }).then((response) => {
+        console.log(response);
+        // TODO: show proper response in the frontend
+        // TODO: clear/reset form
+      });
     },
   },
 };
