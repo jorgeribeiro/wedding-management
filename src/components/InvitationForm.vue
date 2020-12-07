b57da5e106a7e32270b458e4dc445a9cc05c1345
Jorge Ribeiro <joorgemelo@gmail.com>
2020-12-07 22:36:57 +0400

    Add validation and processing state to the invitation form

--- src/components/InvitationForm.vue
+++ src/components/InvitationForm.vue
@@ -1,6 +1,11 @@
 <template>
-  <v-form ref="form">
-    <v-card elevation="1" max-width="500" class="mx-auto">
+  <v-form ref="form" lazy-validation :disabled="disabledState">
+    <v-card
+      :loading="processingState"
+      elevation="1"
+      max-width="500"
+      class="mx-auto"
+    >
       <v-card-title>
         <span class="headline">Nomes dos convidados</span>
       </v-card-title>
@@ -16,13 +21,31 @@
         ></v-text-field>
       </v-card-text>
 
-      <v-card-actions>
-        <v-btn @click="addNameTextField"> + nome </v-btn>
-        <v-btn v-show="showRemoveButton" @click="removeNameTextField">
+      <v-card-actions class="pa-5">
+        <v-btn @click="addNameTextField" :disabled="disabledState">
+          + nome
+        </v-btn>
+        <v-btn
+          v-show="showRemoveButton"
+          @click="removeNameTextField"
+          :disabled="disabledState"
+        >
           - nome
         </v-btn>
-        <v-btn color="primary" @click="save"> Salvar </v-btn>
+        <v-btn color="primary" @click="save" :disabled="disabledState">
+          Salvar
+        </v-btn>
       </v-card-actions>
+
+      <v-alert type="success" v-show="showSuccessAlert">
+        Convite adicionado!
+      </v-alert>
+      <v-alert type="warning" v-show="showWarningAlert">
+        Não é permitido adicionar um convite com nomes vazios.
+      </v-alert>
+      <v-alert type="error" v-show="showErrorAlert">
+        Ocorreu um erro inesperado ao adicionar este convite.
+      </v-alert>
     </v-card>
   </v-form>
 </template>
@@ -48,7 +71,12 @@ export default {
         },
       ],
       numberOfTextFields: 2,
-      showRemoveButton: false,
+      disabledState: false,
+      processingState: false,
+      showRemoveButton: true,
+      showSuccessAlert: false,
+      showWarningAlert: false,
+      showErrorAlert: false,
       nameRules: [(v) => !!v || "Campo obrigatório"],
     };
   },
@@ -79,18 +107,46 @@ export default {
 
     save() {
       let family = [];
-
-      this.textFields.forEach(function(entry) {
-        family.push(entry.value);
+      let inputValidated = true;
+      this.textFields.forEach(function (entry) {
+        let name = entry.value.trim();
+        if (name === "") {
+          inputValidated = false;
+        }
+        family.push(name);
       });
 
-      axios.post(process.env.VUE_APP_API_URL + "/invitation", {
-        "family": family,
-      }).then((response) => {
-        console.log(response);
-        // TODO: show proper response in the frontend
-        // TODO: clear/reset form
-      });
+      if (inputValidated) {
+        this.triggerProcessingState();
+
+        axios
+          .post(process.env.VUE_APP_API_URL + "/invitation", {
+            family: family,
+          })
+          .then(() => {
+            this.showSuccessAlert = true;
+          })
+          .catch(() => {
+            this.showErrorAlert = true;
+          })
+          .finally(() => {
+            this.disabledState = this.processingState = false;
+            setTimeout(
+              () => (this.showSuccessAlert = this.showErrorAlert = false),
+              4000
+            );
+            this.$refs.form.reset();
+          });
+      } else {
+        this.$refs.form.validate();
+        this.showWarningAlert = true;
+        setTimeout(() => (this.showWarningAlert = false), 4000);
+      }
+    },
+
+    triggerProcessingState() {
+      this.disabledState = this.processingState = true;
+      this.showSuccessAlert = this.showWarningAlert = this.showErrorAlert = false;
     },
   },
 };
